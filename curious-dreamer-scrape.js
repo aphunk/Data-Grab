32de4e5d5f0582b9e2201392877c40ce522e911d
Amy Phung <phung.amy85@gmail.com>
2019-07-10 22:46:57 -0700

    add loop to iterate over web pages

--- curious-dreamer-scrape.js
+++ curious-dreamer-scrape.js
@@ -3,20 +3,26 @@ const cheerio = require('cheerio')
 
 const BASE_URL = 'http://www.thecuriousdreamer.com/dreamdictionary/symbol/'
 
-const url = 'http://www.thecuriousdreamer.com/dreamdictionary/symbol/667'
+// const url = 'http://www.thecuriousdreamer.com/dreamdictionary/symbol/2390'
 
-axios.get(url)
-  .then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
+let i = 0;
+while (i < 20) {
+  let url = BASE_URL + i;
 
-    let words = [];
+  axios.get(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-    $('.span7').each(function (i, e) {
-      words[i] = {
-        "word": $(this).find('.term').text().trim(),
-        "meaning": $(this).find('.span4').text().trim(),
-      }
+      let words = [];
+
+      $('.span7').each(function (i, e) {
+        words[i] = {
+          "word": $(this).find('.term').text().trim(),
+          "meaning": $(this).find('.span4').text().trim(),
+        }
+      })
       console.log(words)
     })
-  })
+  i++;
+}
